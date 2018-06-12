bd57cc35d41a411b08dcb43b85182351362ff0d9
Arias <you@example.com>
2018-06-12 15:03:47 -0500

    benis

--- js/4js/js.js
+++ js/4js/js.js
@@ -1,18 +1,16 @@
-var casillas = document.getElementsByClassName("gato");
 var elementoTemporal ;
 let turno = 'x'
-var  = []
-let i = 0;
+
 function test(element){
-
     
-
-
+    
+    
+    
     if( element.innerHTML === ""){
-    elementoTemporal = element.id
-    console.log(elementoTemporal)
+        elementoTemporal = element.id
+        console.log(elementoTemporal)
         console.log("si")
-
+        
         if (turno === 'x') {
             turno  ='O'
             document.getElementById("turno").innerHTML = "Turno de O"
@@ -26,15 +24,37 @@ function test(element){
             element.innerText = "X"
         }
         console.log("el turno es de", turno)
-        
-        for (let i = 0; i < casillass.length; i++) {
-            const element = array[i];
-            
+        inter(element,turno)
+
+
+        function inter (element, turno ){
+        var casillas = document.getElementsByClassName("gato");
+        let  tiros = []
+        for (let i = 0; i < casillas.length; i++) {
+                tiros.push(casillas[i].innerText )
+                
+            }
+            console.log(tiros)
+      
         }
+        
         
     }else{
         return
     }
-    console.log(casillas)
+        function compararCasilla(tiros, casilla1, casilla2, casilla3){
 
+
+
+        }
+       // 123- 456 - 189 // 147 - 258 - 789 // 159 - 357
+
+        compararCasilla(tiros,1,2,3 )
+        compararCasilla(tiros, 4,5,6 )
+        compararCasilla(tiros, , , )
+        compararCasilla(tiros, , , )
+        compararCasilla(tiros, , , )
+        compararCasilla(tiros, , , )
+        compararCasilla(tiros, , , )
+        compararCasilla(tiros, , , )
 }
